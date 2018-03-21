e9723f588ab625e70a99d092dac9d01783179d2e
simon125 <szymqwerty@gmail.com>
2018-03-21 12:35:53 +0100

    almost ended nav, added ol and hover for it

--- sticky.js
+++ sticky.js
@@ -1,4 +1,26 @@
+    
+$(document).ready(function(){
 
+
+    var NavY = $('.nav').offset().top
+    
+    var stickyNav = function(){
+        var ScrollY = $(window).scrollTop()
+    
+        if(ScrollY > NavY) {
+            $('.nav').addClass('sticky')
+        } else {
+            $('.nav').removeClass('sticky')
+        }
+        }
+    
+        stickyNav()
+    
+        $(window).scroll(function(){
+            stickyNav()
+        })
+    
+    })
 
 
 
